7799e8d85ae80f7e5a70f8189c6f2e709d425654
ADEL BOUATTA <83227627+adelwebdev@users.noreply.github.com>
2022-07-09 22:51:42 +0300

    Add files via upload

---
+++ codeSwiift/codeSwiiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  codeSwiiftApp.swift
+//  codeSwiift
+//
+//  Created by BOUATTA ADEL on 09.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct codeSwiiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
